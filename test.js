7d54eea61a4a6083467170ea99b6013eb81e558e
Daniel Rasmussen <daniel.rasmussen@bonniernews.se>
2023-03-23 10:44:59 +0100

    wip recreated error

--- test.js
+++ test.js
@@ -3,6 +3,7 @@
 "use strict";
 
 const http = require("http");
+// TODO: Göra en lista av olika typer av längd för att utöka testet.
 const SERVER_DELAY = 3000; // 3 seconds delay
 
 const server = http.createServer((req, res) => {
@@ -14,13 +15,15 @@ const server = http.createServer((req, res) => {
     });
 
     req.on("end", () => {
+      // console.time("1337 response took: ");
       const data = JSON.parse(body);
       // Do something with the data...
-      console.log("DATA: ", data);
+      console.log("1337 parsed DATA: ", data);
       setTimeout(() => {
         res.writeHead(200, { "Content-Type": "application/json" });
         res.write(JSON.stringify({ message: "Data received", date: Date.now() }));
         res.end();
+        // console.timeEnd("1337 response took: ");
       }, SERVER_DELAY);
     });
   }
@@ -36,8 +39,30 @@ const url = "http://localhost:1337";
 const behavior = {
   httpMethod: "POST",
   // cache: null,
-  socket: 1000,
-  request: 1500,
+
+  /**
+   * Socket must be less han request and both options needs to be in place.
+   * TODO:
+   *  - lägga till någon typ av validering av timeout?
+   *  - alternativt lägga på ett felmeddelande att med timeout.socket = x och timeout.requext = y
+   *  så för väntar du dig att svaret från servern ska alltid vara mindre än y och mindre x.
+   */
+
+  timeout: {
+    socket: 3500,
+    request: 4000,
+  },
+
+  /**
+   * TODO:
+   *  - Om jag inte vet hur lång tid servern svarar, vad ska jag ha för options då?
+   */
+  // retry: {
+  //   limit: 3, // The maximum amount of times to retry the request.
+  //   methods: [ "POST" ], // The HTTP methods that should be retried.
+  //   statusCodes: [ 408, 500, 502, 503, 504 ], // The HTTP status codes that should be retried.
+  //   maxRetryAfter: 4000, // The maximum amount of time in milliseconds that the request should be retried after.
+  // },
 };
 
 const { fetch } = fetchBuilder(behavior);
@@ -59,13 +84,13 @@ async function testFetchTimeout() {
 
     elapsed = date - elapsed;
     if (elapsed > behavior.request) {
-      console.log(`Elapsed time (${elapsed}ms) is longer than ${behavior.request}`);
+      console.log(`Elapsed time (${elapsed}ms) is longer than ${behavior?.timeout?.request}`);
     } else {
-      console.log(`Elapsed time (${elapsed})ms is shorter than request: ${behavior.request}`);
+      console.log(`Elapsed time (${elapsed})ms is shorter than request: ${behavior?.timeout?.request}`);
     }
   } catch (error) {
-    // console.timeEnd("fetch");
-    console.error(error.message);
+    console.timeEnd("fetch took:");
+    console.log("testFetchTimout error: ", error.message);
   }
 }
 
@@ -73,3 +98,39 @@ testFetchTimeout().then(() => {
   console.log("Test complete");
   process.exit(0);
 });
+
+// const got = require("got");
+
+// async function testGot() {
+//   console.time("got took:");
+//   try {
+//     let elapsed = Date.now();
+//     const response = await Promise.race([
+//       got.post(url, {
+//         body: JSON.stringify({ body: "{ test: 123 }" }),
+//         // timeout: 5000,
+//         // retry: {
+//         //   limit: 3,
+//         //   calculateDelay: ({ attemptCount }) => attemptCount * 1000,
+//         // },
+//       }),
+//     ]);
+//     // console.log(await response);
+//     const { message, date } = JSON.parse(await response.body);
+//     console.log("got res message: ", message);
+//     console.timeEnd("got took:");
+
+//     elapsed = date - elapsed;
+//     if (elapsed > behavior.request) {
+//       console.log(`Elapsed time (${elapsed}ms) is longer than ${behavior.request}`);
+//     } else {
+//       console.log(`Elapsed time (${elapsed})ms is shorter than request: ${behavior.request}`);
+//     }
+//   } catch (error) {
+//     console.log("testGot error: ", error);
+//   }
+//   // const gres = await got.post(url, { body: JSON.stringify({ body: "{ test: 123 }" }) });
+//   // console.log("GOT DATA: ", gres.data);
+// }
+
+// testGot();
